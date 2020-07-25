efb99f860e08a0bd0a4beed0cd20d7ad707d328b
sadness_ojisan <sadness.ojisan@gmail.com>
2020-07-26 02:13:23 +0900

    animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,51 +1,11 @@
 import * as React from "react";
 import { AppProps } from "next/app";
 
-export const BlurContext = React.createContext<{
-  state: React.ReactNode | null;
-  action: React.Dispatch<React.SetStateAction<React.ReactNode | null>>;
-}>(null);
-
 export default function App({ Component, pageProps }: AppProps) {
-  const [modalContents, setOpen] = React.useState<React.ReactNode | null>(null);
+  const [isOpen, setOpen] = React.useState(false);
   return (
     <div>
-      <BlurContext.Provider value={{ state: modalContents, action: setOpen }}>
-        <div>
-          {!modalContents ? (
-            <Component {...pageProps} />
-          ) : (
-            <>
-              <div className="blurWrapper">
-                <Component {...pageProps} />
-              </div>
-              <div className="modalWrapper">{modalContents}</div>
-            </>
-          )}
-        </div>
-      </BlurContext.Provider>
-      <style jsx>
-        {`
-          .blurWrapper {
-            filter: blur(5px);
-            background-color: rgba(0, 0, 0, 0.3);
-            position: fixed;
-            width: 100vw;
-            height: 100vh;
-            // ここで位置を左に固定しなければ白背景色とblurが混じって黒半透明のデザインが滲んで崩れる。
-            top: 0;
-            left: 0;
-          }
-          .modalWrapper {
-            position: fixed;
-            top: 5vh;
-            left: 5vw;
-            width: 90vw;
-            height: 90vh;
-            background-color: white;
-          }
-        `}
-      </style>
+      <Component {...pageProps} />
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,58 @@
 import * as React from "react";
-import { BlurContext } from "../pages/_app";
 
 export default () => {
-  const context = React.useContext(BlurContext);
   const [text, setText] = React.useState("");
-  console.log(text);
+  const [isOpen, setOpen] = React.useState(false);
   return (
     <div>
-      <h2>state全部吹っ飛ばすモーダル</h2>
-      <p>入力した後にmodal開いて閉じると状態が消える</p>
-      <p>text = {text}</p>
-      <input onChange={(e) => setText(e.target.value)}></input>
-      <button
-        onClick={() =>
-          context.action(
-            <div className="modalWrapper">
-              <h2>modal no nakami</h2>
-              <button onClick={() => context.action(null)}>close modal</button>
-            </div>
-          )
-        }
-      >
-        open modal
-      </button>
+      <div className="blurWrapper">
+        <h2>state全部吹っ飛ばすモーダル</h2>
+        <p>入力した後にmodal開いて閉じると状態が消える</p>
+        <p>text = {text}</p>
+        <input onChange={(e) => setText(e.target.value)}></input>
+        <button onClick={() => setOpen(true)}>open modal</button>
+      </div>
+      <div className="modalWrapper">
+        <div className="modalContent">
+          <h2>modal no nakami</h2>
+          <button onClick={() => setOpen(false)}>close modal</button>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .blurWrapper {
+            filter: ${isOpen && "blur(5px)"};
+            position: ${isOpen && "fixed"};
+            width: 100vw;
+            height: 100vh;
+            // ここで位置を左に固定しなければ白背景色とblurが混じって黒半透明のデザインが滲んで崩れる。
+            top: 0;
+            left: 0;
+          }
+          .modalWrapper {
+            transition: opacity 0.5s, visibility 0.5s;
+            opacity: ${!isOpen ? 0 : 1};
+            visibility: ${!isOpen ? "hidden" : "initial"};
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            height: 100vh;
+            background-color: ${isOpen && "rgba(0, 0, 0, 0.3)"};
+          }
+          .modalContent {
+            transition: opacity 0.5s, visibility 0.5s;
+            opacity: ${!isOpen ? 0 : 1};
+            visibility: ${!isOpen ? "hidden" : "initial"};
+            position: fixed;
+            top: 5vh;
+            left: 5vw;
+            width: 90vw;
+            height: 90vh;
+            background-color: white;
+          }
+        `}
+      </style>
     </div>
   );
 };

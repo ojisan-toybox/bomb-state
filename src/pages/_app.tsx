bb2924905d5c41f5357e6fc2b9196cd6cc484212
sadness_ojisan <sadness.ojisan@gmail.com>
2020-07-26 02:15:45 +0900

    refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isOpen, setOpen] = React.useState(false);
   return (
     <div>
       <Component {...pageProps} />
